e49851d89b94d2c01e6b8145fe8998548430d653
Nikhil Nair <niknair31898@gmail.com>
2024-10-25 22:12:13 -0400

    Added placeholders for GitHub scraping

---
+++ gith.py
@@ -0,0 +1,1 @@
+# Contains main function related to GitHub API
---
+++ scrape_github.py
@@ -0,0 +1,8 @@
+# Contains code to scrape GitHub use profile data
+
+# # of repos
+# # of commits
+# Read READMEs of all repos
+# Get % across languages
+# Generate a list of technologies used in the repos
+# Generate summaries of type of projects worked on by the user
